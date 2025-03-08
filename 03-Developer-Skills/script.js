babcf1d265f47af4bb2021b6d2a23907b8b89572
Subhash <ss17480@gmail.com>
2025-03-08 20:03:48 +0530

    Debugging Skills

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*const x = '23';
+
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2025 - birthYear;
+
+console.log(calcAge(1994));
+*/
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    //C)Fix
+    value: Number(prompt('Degree Celsius:')),
+  };
+  console.log(measurement);
+  //B)find
+  console.table(measurement);
+  //console.log(measurement.value);
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+//A) Identify the Bug
+console.log(measureKelvin());
